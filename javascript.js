254a029da6de60999f57f3eb86faf8cbfb79df23
sahilansari07 <sahilansari2493@gmail.com>
2023-03-20 07:18:28 +0530

    my changes

--- javascript.js
+++ javascript.js
@@ -7,7 +7,6 @@ class Book {
     this.pages = pages;
     this.read = read;
   }
-  static count = 1;
 }
 
 function addBookToLibrary() {
@@ -26,12 +25,60 @@ function addBookToLibrary() {
   else {
     var NewBook = new Book(title, author, pages, read);
     myLibrary.push(NewBook);
-    console.log(NewBook);
-    displaybook(NewBook);
+    createcard();
   }
 }
 
-function displaybook(NewBook) {
+let newBookbtn = document.querySelector("#new-book-btn");
+newBookbtn.addEventListener("click", function () {
+  let newBookForm = document.querySelector("#new-book-form");
+  newBookForm.style.display = "block";
+})
+
+document.querySelector("#new-book-form").addEventListener("submit", function (event) {
+  event.preventDefault();
+  addBookToLibrary();
+})
+
+function createcard() {
+  let libraryEl = document.querySelector("#library");
+  libraryEl.innerHTML = "";
+  for (let i = 0; i < myLibrary.length; i++) {
+    let book = myLibrary[i];
+    let bookEl = document.createElement("div");
+    bookEl.setAttribute("class", "displaycard");
+    bookEl.innerHTML = `
+      <div class="card-header">
+        <h3 class="title">${book.title}</h3>
+        <h5 class="author">${book.author}</h5>
+      </div>
+      <div class="card-body">
+        <p>${book.pages} pages</p>
+        <p class="read-status">${book.read ? "Read" : "Not Read Yet"}</p>
+        <button class="remove-btn" onclick="deleteBook(${i})">Remove</button>
+        <button class="toggle-read-btn" onclick="toggleRead(${i})">Toggle Read</button>
+      </div>
+    `;
+    libraryEl.appendChild(bookEl);
+  }
+}
+
+function deleteBook(index) {
+  myLibrary.splice(index, 1);
+  createcard();
+}
+
+Book.prototype.toggleRead = function() {
+  this.read = !this.read;
+}
+
+function toggleRead(index) {
+  myLibrary[index].toggleRead();
+  createcard()
+}
+
+
+/*function displaybook(NewBook) {
   let myTable = document.getElementById("displaytable");
   var row = myTable.insertRow(Book.count);
 
@@ -47,26 +94,102 @@ function displaybook(NewBook) {
   
   button = document.createElement("BUTTON");
   button.innerText = "Delete";
-  button.className = "delbtn";
+  button.setAttribute("id", "delbtn");
   button.value = Book.count;
   row.append(button);
   cell5 = button;
-  /*button.setAttribute("width", "input")*/
+  /*button.setAttribute("width", "input")
   button.setAttribute("onclick", "deletebtn()")
 
   Book.count++;
-}
+}*/
 
 
-function deletebtn() {
 
-  let string = document.getElementsByName("delbtn");
-  console.log(string);
-  let result= string.substr(-1,1);
-  console.log(result);
-  document.getElementById("displaytable").deleteRow(result);
+/*function displaybook(NewBook){
 
-}
+  let para1 = document.createElement("p");
+  para1.setAttribute("id", "inputTitle"+Book.count);
+  para1.innerHTML= NewBook.title;
+  document.body.appendChild(para1);
+
+  let para2 = document.createElement("p");
+  para2.setAttribute("id", "inputAuthor"+Book.count);
+  para2.innerHTML= NewBook.author;
+  document.body.appendChild(para2);
+
+  let para3 = document.createElement("p");
+  para3.setAttribute("id", "inputPages"+Book.count);
+  para3.innerHTML= NewBook.pages;
+  document.body.appendChild(para3);
+
+  let para4 = document.createElement("p");
+  para4.setAttribute("id", "inputRead"+Book.count);
+  para4.innerHTML= NewBook.read;
+  document.body.appendChild(para4);
+
+  Book.count++;
+}*/
+
+/*function displaybook(NewBook) {
+  
+  let tablerow = document.createElement("tr");
+    
+  let para1 = document.createElement("td");
+  para1.setAttribute("id", "inputTitle" + Book.count);
+  para1.innerHTML = NewBook.title;
+  document.getElementById("displaytable").appendChild(para1);
+
+  let para2 = document.createElement("td");
+  para2.setAttribute("id", "inputAuthor" + Book.count);
+  para2.innerHTML = NewBook.author;
+  document.getElementById("displaytable").appendChild(para2);
+
+  let para3 = document.createElement("td");
+  para3.setAttribute("id", "inputPages" + Book.count);
+  para3.innerHTML = NewBook.pages;
+  document.getElementById("displaytable").appendChild(para3);
+
+  let para4 = document.createElement("td");
+  para4.setAttribute("id", "inputRead" + Book.count);
+  para4.innerHTML = NewBook.read;
+  document.getElementById("displaytable").appendChild(para4);
+
+  Book.count++;
+} */
+
+/* function createcard(NewBook) {
+  let libraryEl = document.querySelector("#library");
+  libraryEl.innerHTML = "";
+  for (let i = 0; i < myLibrary.length; i++) {
+
+    let card = document.createElement("div");
+    let titleval = document.createElement("p");
+    let authorval = document.createElement("p");
+    let pagesval = document.createElement("p");
+    let readval = document.createElement("button");
+    let deleteval = document.createElement("button");
+
+    titleval.innerHTML = NewBook.title;
+    authorval.innerHTML = NewBook.author;
+    pagesval.innerHTML = NewBook.pages;
+    readval.innerText = NewBook.read;
+    deleteval.innerText = "Delete";
+
+    libraryEl.appendChild(card);
+    card.appendChild(titleval);
+    card.appendChild(authorval);
+    card.appendChild(pagesval);
+    card.appendChild(readval);
+    card.appendChild(deleteval);
+
+    card.setAttribute("class", "displaycard");
+    card.setAttribute("id", "card");
+    readval.setAttribute("id", "readbutton");
+    deleteval.setAttribute("id", "deletebutton");
+    deleteval.setAttribute("onclick", deletebook(i));
+  }
+} */
 
 
 
